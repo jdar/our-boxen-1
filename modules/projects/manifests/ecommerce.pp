fee528a8372768a4aae20bd499895370d7985372
Darius Roberts <darius.roberts@gmail.com>
2015-12-04 08:47:36 -0800

    wip

--- modules/projects/manifests/ecommerce.pp
+++ modules/projects/manifests/ecommerce.pp
@@ -1,7 +1,9 @@
 class projects::ecommerce {
-  ruby::version { '2.2.2': }
-  
   $ecommerce_dir = "${boxen::config::srcdir}/ecommerce"
   repository { $ecommerce_dir:
-    source => "git@heroku.com:warm-peak-7381.git"  } 
+    source => "git@heroku.com:warm-peak-7381.git"  
+  } 
+  ruby::local { $ecommerce_dir:
+    version => '2.2.2'
+  } 
 }
